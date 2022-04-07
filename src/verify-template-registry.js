0cc9bf07853f4e7ca5c8430d075a06cd48c82251
Serhii Litviachenko <slitviachenko@gmail.com>
2022-04-07 11:21:43 -0500

    const octokit = new github.getOctokit(githubToken);

--- src/verify-template-registry.js
+++ src/verify-template-registry.js
@@ -1,11 +1,37 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import { getRegistry } from "./registry.js";
 
-const myArgs = process.argv.slice(2);
-const issueNumber = myArgs[0];
-console.log('issueNumber', issueNumber);
+const GITHUB_REPO = 'aio-template-submission';
+const GITHUB_REPO_OWNER = 'adobe';
 
-const registry = getRegistry();
-for (const item of registry) {
-    console.log('item', item);
-}
+(async () => {
+    try {
+        const myArgs = process.argv.slice(2);
+        const issueNumber = myArgs[0];
+        const githubToken = myArgs[1];
+        const octokit = new github.getOctokit(githubToken);
+        console.log('issueNumber', issueNumber);
+
+        const registry = getRegistry();
+        for (const item of registry) {
+            const packageName = item.name;
+            const gitHubUrl = item.links.npm;
+            const npmUrl = item.links.github;
+            await octokit.rest.issues.createComment({
+                'owner': GITHUB_REPO_OWNER,
+                'repo': GITHUB_REPO,
+                'issue_number': issueNumber,
+                'body': {
+                    packageName,
+                    npmUrl,
+                    gitHubUrl
+                }
+            });
+            console.log('item', item);
+        }
+    } catch (e) {
+        core.setOutput('error', `:x: ${e.message}`);
+        throw e;
+    }
+})();
