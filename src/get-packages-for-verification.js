c686d2cd1e5a79adef45f7d4357267294d2c4304
Serhii Litviachenko <slitviachenko@gmail.com>
2022-04-08 15:17:11 -0500

    extended get-packages-for-verification.js

--- src/get-packages-for-verification.js
+++ src/get-packages-for-verification.js
@@ -1,9 +1,20 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import { getRegistry } from "./registry.js";
+
+const GITHUB_REPO = 'aio-template-submission';
+// const GITHUB_REPO_OWNER = 'adobe';
+const GITHUB_REPO_OWNER = 'slitviachenko';
 
 (async () => {
     try {
-        const packages = {
+        const githubToken = process.env.GITHUB_TOKEN;
+        const myArgs = process.argv.slice(2);
+        const issueNumber = myArgs[0];
+        const octokit = new github.getOctokit(githubToken);
+        console.log('issueNumber', issueNumber);
+
+        const data = {
             packages: []
         };
         const registry = getRegistry();
@@ -11,13 +22,33 @@ import { getRegistry } from "./registry.js";
             const packageName = item.name;
             const packageNpmUrl = item.links.npm;
             const packageGithubUrl = item.links.github;
-            packages.packages.push({
-                packageName,
-                packageNpmUrl,
-                packageGithubUrl
+            data.packages.push({
+                'packageName': packageName,
+                'packageNpmUrl': packageNpmUrl,
+                'packageGithubUrl': packageGithubUrl,
+                'action': 'update'
             });
         }
-        core.setOutput('list', JSON.stringify(packages));
+        if (data.packages.length > 0) {
+            let comment = 'We are going to:\n';
+            for (const item in data.packages) {
+                comment += `- ${item.action} "${item.packageName}"`
+            }
+            await octokit.rest.issues.createComment({
+                'owner': GITHUB_REPO_OWNER,
+                'repo': GITHUB_REPO,
+                'issue_number': issueNumber,
+                'body': comment
+            });
+        } else {
+            await octokit.rest.issues.createComment({
+                'owner': GITHUB_REPO_OWNER,
+                'repo': GITHUB_REPO,
+                'issue_number': issueNumber,
+                'body': ':white_check_mark: All template packages in Template Registry contain the latest information. Nothing to update.'
+            });
+        }
+        core.setOutput('data', JSON.stringify(data));
     } catch (e) {
         core.setOutput('error', `:x: ${e.message}`);
         throw e;
